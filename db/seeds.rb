6fa0c000e96befd9a720ffa72a777cea063bd62e
DebyGrey <demifashoro@gmail.com>
2023-10-13 01:48:54 +0100

    Implement API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting_msgs = ['Hello World', 'Welcome to Rails React', 'Hello Webpack', 'Hello Redux', 'Hello World']
+
+greeting_msgs.each do |msg|
+  Message.create(greeting: msg)
+end
